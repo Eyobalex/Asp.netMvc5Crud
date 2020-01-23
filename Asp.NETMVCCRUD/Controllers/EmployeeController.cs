54f4d9d8c4aafe2dcc3aca6535413d2d1cc940b6
Eyobalex <=>
2020-01-23 22:57:01 +0300

    completed

---
+++ Asp.NETMVCCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp.NETMVCCRUD.Models;
+using System.Data.Entity;
+
+namespace Asp.NETMVCCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Employee());
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.Employees.Where(x => x.EmployeeID==id).FirstOrDefault<Employee>());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (emp.EmployeeID == 0)
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                Employee emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ Asp.NETMVCCRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asp.NETMVCCRUD.MvcApplication" Language="C#" %>
